a9da10fa4892d139fd82768fc4b6e4cfd20e03f8
K <97007ken@gmail.com>
2023-02-11 20:07:01 +0800

    Update - add join_linebot.vue , join_linebot.html but only appearance,add image folder for images

---
+++ src/join_linebot.vue
@@ -0,0 +1,13 @@
+
+<script setup>
+  import bar from './components/bar.vue'
+  import join_linebot from './components/join_linebot.vue'
+</script>
+<template>
+  <bar />
+  <join_linebot />
+</template>
+
+<style scoped>
+
+</style>
