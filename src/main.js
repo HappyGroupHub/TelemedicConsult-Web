f9471e3c3267e905e671999a8b4b244abb25e643
K <97007ken@gmail.com>
2023-02-12 20:20:54 +0800

    Update - add reservation.vue and reservation.html but only appearance

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import './style.css'
 import App from './patient_login.vue'
 import patient_base from './write_patient_base1.vue'
 import join_linebot from './join_linebot.vue'
+import reservation from './reservation.vue'
+
 
 createApp(App).mount('#app')
 createApp(patient_base).mount('#patient_base')
 createApp(join_linebot).mount('#join_linebot')
+createApp(reservation).mount('#reservation')
---
+++ src/reservation.vue
@@ -0,0 +1,15 @@
+
+<script setup>
+  import bar from './components/bar.vue'
+  import reservation from './components/reservation.vue'
+
+</script>
+<template>
+  <bar />
+  <reservation />
+</template>
+
+
+<style scoped>
+
+</style>
