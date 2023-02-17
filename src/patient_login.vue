2dbaaf8372c8802dd63a749bcef3501a7140d424
K <97007ken@gmail.com>
2023-02-17 15:10:40 +0800

    Update - add banner_login.vue in patient_login.vue

--- src/patient_login.vue
+++ src/patient_login.vue
@@ -1,11 +1,14 @@
 <script setup>
   import bar from './components/bar.vue'
   import login from './components/login.vue'
+  import banner_login from './components/banner_login.vue'
+  import Banner_login from "./components/banner_login.vue";
 
 </script>
 
 <template>
   <bar />
+  <banner_login/>
   <login />
 </template>
 
