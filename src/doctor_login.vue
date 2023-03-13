05e456c39f3866a069e5aa0cd219fdc3e1a89079
K <97007ken@gmail.com>
2023-03-13 16:57:27 +0800

    Update - add doctor_login.html

---
+++ src/doctor_login.vue
@@ -0,0 +1,18 @@
+
+<script setup>
+import doctor_login from "./components/doctor_login.vue"
+import banner_doctor_login from "./components/banner_doctor_login.vue";
+import bar from "./components/bar.vue"
+import Bar from "./components/bar.vue";
+</script>
+
+<template >
+  <bar></bar>
+  <banner_doctor_login/>
+  <doctor_login/>
+</template>
+
+<style scoped>
+
+</style>
+
