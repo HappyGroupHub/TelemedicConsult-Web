b8dbac9c4f48c82e1013d468cd133f36b04a510c
Tingwei <lydia9017@gmail.com>
2023-03-20 00:18:51 +0800

    Update - finish write patient

--- backend/sql_connection.js
+++ backend/sql_connection.js
@@ -32,3 +32,48 @@ function register_patient(name, id, sex, birthday, blood_type, ic_card_number, p
 register_patient('測試用', 'A123456789', '男', '1999-01-01', 'A', '0123456789012',
     '0912345678', '台北市中正區', '170', '55', '測試用', '父親',
     '0912345678');
+
+//import axios from "axios";
+// import { ref } from "vue";
+//
+// const name = ref("");
+// const id = ref("");
+// const birthday = ref("");
+// const ice_contact = ref("");
+// const sex = ref("");
+// const ice_phone = ref("");
+// const ice_relation = ref("");
+// const ic_card_number = ref("");
+// const phone_number = ref("");
+// const height = ref("");
+// const weight = ref("");
+// const blood_type = ref("");
+// const address = ref("");
+//
+// function register_patient() {
+//   let config = { headers: {
+//       'Content-Type': 'application/json',
+//       'Access-Control-Allow-Origin': '*'}
+//   }
+//   axios.post('http://127.0.0.1:5000/register_patient', {
+//     name: name.value,
+//     id: id.value,
+//     sex: sex.value,
+//     birthday: birthday.value,
+//     blood_type: blood_type.value,
+//     ic_card_number: ic_card_number.value,
+//     phone_number: phone_number.value,
+//     address: address.value,
+//     height: height.value,
+//     weight: weight.value,
+//     ice_contact: ice_contact.value,
+//     ice_relation: ice_relation.value,
+//     ice_phone: ice_phone.value
+//   }, config)
+//       .then(res => {
+//         console.log(res)
+//       })
+//       .catch(err => {
+//         console.log(err)
+//       });
+// }
--- src/components/writing_patient.vue
+++ src/components/writing_patient.vue
@@ -3,7 +3,7 @@
     <br>
     <h2>初診基本資料</h2>
     <div class="basic">
-      <form>
+      <form @submit.prevent="register_patient">
         <div id="left">
             姓名<br>
             <input v-model="name" type="text" id="name" name="name" required="required" />
@@ -14,9 +14,12 @@
             生日<br>
             <input v-model="birthday" type="date"  id="birthday" name="birthday" required="required" />
           <br>
+          <session class="form_session">
             生理性別<br>
             <input v-model="sex"  name="sex" type="radio" value="男"  required="required">男
             <input v-model="sex"  name="sex" type="radio" value="女"  required="required">女
+          </session>
+
           <br>
             緊急聯絡人姓名<br>
             <input v-model="ice_contact"  id="ice_contact" name="ice_contact" required="required">
@@ -37,19 +40,26 @@
             手機號碼<br>
             <input v-model="phone_number" name="phone_number"  required="required" type="text" maxlength="10" pattern="09\d{8}" placeholder="09xxxxxxxx">
           <br>
+          <session class="form_session">
             身高<br>
             <input v-model="height" type="text"  id="height" name="height" />公分
+          </session>
+
           <br>
+          <session class="form_session">
             體重<br>
             <input v-model="weight" type="text" id="weight" name="weight" />公斤
+          </session>
           <br>
+          <session class="form_session">
             血型<br>
             <input v-model="blood_type" type="text"  id="blood_type" name="blood_type" required="required" />型
+          </session>
           <br>
             地址<br>
             <input v-model="address" type="text"  id="address" name="address" required="required" />
           <br>
-          <button @click="register_patient"  id="check_writing" type="submit"  style="width:150px;height:50px;background-color: #00317B;color:white;text-align: center;border:0" >確認填寫</button>
+          <button   id="check_writing" type="submit"  style="width:150px;height:50px;background-color: #00317B;color:white;text-align: center;border:0" >確認填寫</button>
           <br>
         </div>
     </form>
@@ -59,50 +69,56 @@
 </template>
 
 <script setup>
+
 import axios from "axios";
-import { ref } from "vue";
+ import { ref } from "vue";
 
-const name = ref("");
-const id = ref("");
-const birthday = ref("");
-const ice_contact = ref("");
-const sex = ref("");
-const ice_phone = ref("");
-const ice_relation = ref("");
-const ic_card_number = ref("");
-const phone_number = ref("");
-const height = ref("");
-const weight = ref("");
-const blood_type = ref("");
-const address = ref("");
+ const name = ref("");
+ const id = ref("");
+ const birthday = ref("");
+ const ice_contact = ref("");
+ const sex = ref("");
+ const ice_phone = ref("");
+ const ice_relation = ref("");
+ const ic_card_number = ref("");
+ const phone_number = ref("");
+ const height = ref("");
+ const weight = ref("");
+ const blood_type = ref("");
+ const address = ref("");
 
-  function register_patient() {
-  let config = { headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'}
-  }
-  axios.post('http://127.0.0.1:5000/register_patient', {
-    name: name.value,
-    id: id.value,
-    sex: sex.value,
-    birthday: birthday.value,
-    blood_type: blood_type.value,
-    ic_card_number: ic_card_number.value,
-    phone_number: phone_number.value,
-    address: address.value,
-    height: height.value,
-    weight: weight.value,
-    ice_contact: ice_contact.value,
-    ice_relation: ice_relation.value,
-    ice_phone: ice_phone.value
-  }, config)
-      .then(res => {
+ function register_patient() {
+   let config = { headers: {
+       'Content-Type': 'application/json',
+       'Access-Control-Allow-Origin': '*'}
+   }
+   axios.post('http://127.0.0.1:5000/register_patient', {
+     name: name.value,
+     id: id.value,
+     sex: sex.value,
+     birthday: birthday.value,
+     blood_type: blood_type.value,
+     ic_card_number: ic_card_number.value,
+     phone_number: phone_number.value,
+     address: address.value,
+     height: height.value,
+     weight: weight.value,
+     ice_contact: ice_contact.value,
+     ice_relation: ice_relation.value,
+     ice_phone: ice_phone.value
+   }, config)
+       .then(res => {
         console.log(res)
       })
       .catch(err => {
         console.log(err)
-      });
-}
+       });
+ }
+
+
+
+
+
 </script>
 
 <style >
