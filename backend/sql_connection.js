feaa3ea23144fa93bd7299837537e935e763de72
LD <chiyc88@gmail.com>
2023-05-11 15:04:37 +0800

    Update - you can now retrieve list of patients by entering clinic_id from backend

--- backend/sql_connection.js
+++ backend/sql_connection.js
@@ -239,21 +239,41 @@ function get_doctor_clinic_list(doc_id) {
         });
 }
 
+
+function get_patients_by_clinic_id(clinic_id) {
+    let config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'localhost:5000'
+        }
+    }
+    axios.post('http://127.0.0.1:5000/get_patients_by_clinic_id', {
+        clinic_id: clinic_id
+    }, config)
+        .then(response => {
+            console.log(response)
+        })
+        .catch(err => {
+            console.log(err)
+        });
+}
+
 //get_patient_info_by_id('F130937605')
 // if_patient_registered_line('A123456789')
 // update_patient_info_by_id('F130937605', '0912345678', '台北市中山區', 170, 60, '王小明', '父親', '0912345678')
 // register_patient('王小明', 'F130937605', '男', '1990-01-01', 'A', 'A123456789', '0912345678', '台北市中山區', 170, 60, '王小明', '父親', '0912345678')
 // check_if_time_have_clinic('2023-05-05', '早')
 // update_clinic_status('2', {
-//     'start_time': '2023-4-27 14:10',
-//     'end_time': '2023-4-27 14:20',
-//     'link': 'https://google.com',
-//     'total_appointment': '100',
-//     'progress': '88'
+    // 'start_time': '2023-4-27 14:10',
+    // 'end_time': '2023-4-27 14:20',
+    // 'link': 'https://google.com',
+    // 'total_appointment': '100',
+    // 'progress': '88'
 // })
 // make_appointment('2', 'F130937605')
 // cancel_appointment('F130937605', '2')
 // get_clinic_info('2')
 // get_patient_appointment_with_clinic_id('N126761037', '2')
 // doctor_login('12345', 6969)
-get_doctor_clinic_list('12345')
+// get_doctor_clinic_list('12345')
+get_patients_by_clinic_id('2')
